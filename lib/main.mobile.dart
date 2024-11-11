3482dbcf538fcddb0877720b589308d4ca551c43
hmziqrs <hmziqrs@gmail.com>
2024-11-11 17:32:34 +0500

    Fixed firebase config

--- lib/main.mobile.dart
+++ lib/main.mobile.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:invmovieconcept1/firebase_options.dart';
 
 import 'Utils.dart';
 import 'native/Cache.dart';
@@ -14,7 +15,9 @@ import 'Navigator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Cache.init();
 
   App.showAds = Utils.isMobile();
--- lib/main.web.dart
+++ lib/main.web.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:invmovieconcept1/firebase_options.dart';
 
 import 'native/Cache.dart';
 
@@ -9,6 +11,9 @@ import 'Navigator.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Cache.init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   final analyticsObserver = FirebaseAnalyticsObserver(
     analytics: FirebaseAnalytics.instance,
   );
